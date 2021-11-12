378822c0f4d81dba5e490942266304d4edd1004b
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-11-11 17:03:55 -0800

    progress

--- flashbake/app.js
+++ flashbake/app.js
@@ -1,13 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 10732
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.post('/', (req, res) => {
+app.post('/injection/operation', (req, res) => {
   console.log(req);
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
