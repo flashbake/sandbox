fa9654edc336c3d33e204ad3db0725ba350cc8bc
Nicolas Ochem <nicolas.ochem@gmail.com>
2021-11-15 18:00:56 -0800

    why not both

--- flashbake/app.js
+++ flashbake/app.js
@@ -1,11 +1,30 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
-var http_proxy_middleware_1 = require("http-proxy-middleware");
 var utils_1 = require("@taquito/utils");
+var http = __importStar(require("http"));
 var bodyParser = require('body-parser');
 var app = (0, express_1.default)();
 var port = 10732;
@@ -18,12 +37,30 @@ app.post('/injection/operation', function (req, res) {
     console.log(opHash);
     res.json(opHash);
 });
-var mempoolProxy = (0, http_proxy_middleware_1.createProxyMiddleware)({
-    target: 'http://tezos-node-rpc:8732',
-    changeOrigin: true
+// mempool queries are handled directly
+app.get('/chains/main/mempool/monitor_operations', function (req, res) {
+    http.get('http://tezos-node-rpc:8732/chains/main/mempool/monitor_operations', function (resp) {
+        res.setHeader('Content-Type', 'application/json');
+        res.removeHeader("Connection");
+        // A chunk of data has been received.
+        resp.on('data', function (chunk) {
+            console.log(chunk.toString());
+            res.write(chunk);
+        });
+        // octez has ended the response (because a new head has been validated)
+        resp.on('end', function () {
+            res.end();
+        });
+    }).on("error", function (err) {
+        console.log("Error: " + err.message);
+    });
 });
-app.use('/chains/main/mempool/monitor_operations', mempoolProxy);
-app.use('/chains/main/chain_id', mempoolProxy);
-app.listen(port, function () {
+// every other query is directly proxied to the node
+//const mempoolProxy = createProxyMiddleware({
+//    target: 'http://tezos-node-rpc:8732',
+//    changeOrigin: true });
+//app.use('/**', mempoolProxy);
+var server = app.listen(port, function () {
     console.log("Flashbake relay and endpoint listening at http://localhost:" + port);
 });
+server.setTimeout(500000);
--- flashbake/app.ts
+++ flashbake/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { createProxyMiddleware, Filter, Options, RequestHandler } from 'http-proxy-middleware';
 import { encodeOpHash } from "@taquito/utils";
+import * as http from "http";
+
 var bodyParser = require('body-parser');
 
 const app = express()
@@ -17,13 +19,36 @@ app.post('/injection/operation', (req, res) => {
   res.json(opHash);
 })
 
+// mempool queries are handled directly
+app.get('/chains/main/mempool/monitor_operations', (req, res) => {
 
-const mempoolProxy = createProxyMiddleware({
-    target: 'http://tezos-node-rpc:8732',
-    changeOrigin: true });
+  http.get('http://tezos-node-rpc:8732/chains/main/mempool/monitor_operations', (resp) => {
 
-app.use('/chains/main/mempool/monitor_operations', mempoolProxy);
-app.use('/chains/main/chain_id', mempoolProxy);
-app.listen(port, () => {
+    res.setHeader('Content-Type', 'application/json');
+    res.removeHeader("Connection");
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+      console.log(chunk.toString());
+      res.write(chunk);
+    });
+  
+    // octez has ended the response (because a new head has been validated)
+    resp.on('end', () => {
+      res.end();
+    });
+  
+  }).on("error", (err) => {
+    console.log("Error: " + err.message);
+  });
+})
+
+// every other query is directly proxied to the node
+//const mempoolProxy = createProxyMiddleware({
+//    target: 'http://tezos-node-rpc:8732',
+//    changeOrigin: true });
+//app.use('/**', mempoolProxy);
+
+let server = app.listen(port, () => {
   console.log(`Flashbake relay and endpoint listening at http://localhost:${port}`)
 })
+server.setTimeout(500000);
