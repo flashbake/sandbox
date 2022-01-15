51bdaca3a86db592343dc8da9faf51751de78e91
Daniel Feygin <daniel.feygin@gmail.com>
2022-01-15 15:26:48 -0300

    implement relay service

--- flashbake-relay/app.ts
+++ flashbake-relay/app.ts
@@ -1,8 +1,31 @@
 import express from 'express';
+import { InMemoryRegistryService, RegistryService } from "@flashbake/relay";
+import { HttpRelay } from '@flashbake/relay';
 
-const app = express();
-const port = 11732;
-const server = app.listen(port, () => {
-  console.log(`Hello world, Flashbake relay placeholder is running`);
-})
-server.setTimeout(500000);
+
+function startRelay(port: number, rpcApiUrl: string, bakerAddress: string, bakerEndpointUrl: string): HttpRelay {
+  const bakerRegistry = new InMemoryRegistryService();
+  bakerRegistry.setEndpoint(bakerAddress, bakerEndpointUrl);
+
+  const relayApp = express();
+  const relayer = new HttpRelay(relayApp, bakerRegistry, rpcApiUrl);
+  const server = relayApp.listen(port, () => {
+    console.log(`Flashbake relay started on http://localhost:${port}`)
+    console.log(`Relaying to ${bakerEndpointUrl} for address ${bakerAddress}`)
+  })
+  server.setTimeout(500000);
+
+  return relayer;
+}
+
+function main() {
+  const relayPort = 10732;
+  const bakerPort = 11732;
+  const rpcApiUrl = 'http://localhost:8732';
+  const bakerAddress = 'tz1THLWsNuricp4y6fCCXJk5pYazGY1e7vGc';
+  const bakerEndpointUrl = `http://localhost:${bakerPort}/flashbake/bundle`;
+
+  startRelay(relayPort, rpcApiUrl, bakerAddress, bakerEndpointUrl);
+}
+
+main();
