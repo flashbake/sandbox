3068099519b53527a59249cd1bd9cd9bd3a00459
Daniel Feygin <daniel.feygin@gmail.com>
2022-01-20 19:47:26 -0300

    expand relay to two hard-coded bakers

--- flashbake-relay/app.ts
+++ flashbake-relay/app.ts
@@ -3,15 +3,18 @@ import { InMemoryRegistryService, RegistryService } from "@flashbake/relay";
 import { HttpRelay } from '@flashbake/relay';
 
 
-function startRelay(port: number, rpcApiUrl: string, bakerAddress: string, bakerEndpointUrl: string): HttpRelay {
+function startRelay(port: number, rpcApiUrl: string, bakers: Map<string,string>): HttpRelay {
   const bakerRegistry = new InMemoryRegistryService();
-  bakerRegistry.setEndpoint(bakerAddress, bakerEndpointUrl);
+  
+  bakers.forEach((bakerEndpointUrl, bakerAddress) => {
+    bakerRegistry.setEndpoint(bakerAddress, bakerEndpointUrl);
+    console.log(`Relaying to ${bakerEndpointUrl} for address ${bakerAddress}`)
+  })
 
   const relayApp = express();
   const relayer = new HttpRelay(relayApp, bakerRegistry, rpcApiUrl);
   const server = relayApp.listen(port, () => {
     console.log(`Flashbake relay started on http://localhost:${port}`)
-    console.log(`Relaying to ${bakerEndpointUrl} for address ${bakerAddress}`)
   })
   server.setTimeout(500000);
 
@@ -22,10 +25,11 @@ function main() {
   const relayPort = 10732;
   const bakerPort = 11732;
   const rpcApiUrl = 'http://localhost:8732';
-  const bakerAddress = 'tz1THLWsNuricp4y6fCCXJk5pYazGY1e7vGc';
-  const bakerEndpointUrl = `http://localhost:${bakerPort}/flashbake/bundle`;
-
-  startRelay(relayPort, rpcApiUrl, bakerAddress, bakerEndpointUrl);
+  const bakers: Map<string,string> = new Map<string,string>([
+    ['tz1THLWsNuricp4y6fCCXJk5pYazGY1e7vGc', `http://tezos-baking-node-0.tezos-baking-node:${bakerPort}/flashbake/bundle`],
+    ['tz1RUyvixHtL1Pwwg41ZB9WKMWTQgC7gs3Z6', `http://tezos-baking-node-1.tezos-baking-node:${bakerPort}/flashbake/bundle`]
+  ]);
+  startRelay(relayPort, rpcApiUrl, bakers);
 }
 
 main();
